af4f61cc5fb0e99f929187b3bcb8609f9f8905d3
manuelrs31 <maza.mr.mr@gmail.com>
2019-11-01 08:27:38 -0600

    this is a example of servlet and MVC

---
+++ js/script.js
@@ -0,0 +1,17 @@
+/*window.onload = function ()
+{
+	alert("testo");
+}*/
+/*$(
+alert("testo")		);  */
+/*$(document).ready(
+		function()
+		{
+			alert("testo");
+			alert("testo2");
+		}
+);  */
+$(
+	alert("otro Testo"),
+	alert ("otro Testo2")
+	)
